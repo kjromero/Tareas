acfe83a03b54da2f0f74183cb7e5123d0c7584fd
gilberth <garenteria@anutibara.com>
2020-07-05 20:12:22 -0500

    app finished

--- backend/config/index.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
-  
-  module.exports = {
-    PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
-  };
-  
